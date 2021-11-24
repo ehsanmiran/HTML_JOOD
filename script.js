97081b1e2d88be6fad58cd3eec8a98900bbd1f2a
Jonas <jonasengstrom_@hotmail.com>
2021-11-24 12:22:10 +0100

    swiper

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 var swiper = new Swiper(".mySwiper", {
 
+  // Jonas Engström
+  effect: 'coverflow',
+  speed: 400,
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
-  slideActiveClass: "active-slide",
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  coverflowEffect: {
+    rotate: -100,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    bulletActiveClass: 'swiper-pagination-bullet-active',
-  },
+   navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  
+ 
+  
+  
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 130,
+      coverflowEffect: {
+        rotate: -10,
+        stretch: 0,
+        depth: 80,
+      },
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 150,
+      coverflowEffect: {
+        rotate: -5,
+        stretch: -35,
+        depth: 120,
+      },
+    },
+  }
 
 });
-
-
-
-
 
 wow = new WOW(
   {
