f6deb132ee13927b9097320bb7a8df9ce6e97508
Jonas <jonasengstrom_@hotmail.com>
2021-11-24 01:02:18 +0100

    carousel

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
-var swiper = new Swiper(".swiper", {
-  effect: "coverflow",
+var swiper = new Swiper(".mySwiper", {
+
   grabCursor: true,
   centeredSlides: true,
-  loop: true,
   slidesPerView: 1,
-  coverflowEffect: {
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: false,
-  },
+  spaceBetween: 30,
+  loop: true,
+  slideActiveClass: "active-slide",
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-  
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 100,
-      coverflowEffect: {
-        rotate: -25,
-        stretch: 35,
-        depth: 100,
-        modifier: 0.7,
-        slideShadows: false,
-      }
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 60,
-      coverflowEffect: {
-        rotate: -14,
-        stretch: -20,
-        depth: 100,
-        modifier: 1,
-        slideShadows: false,
-      }
-    }
-  }
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    bulletActiveClass: 'swiper-pagination-bullet-active',
+  },
+
 });
+
+
+
+
 
 wow = new WOW(
   {
